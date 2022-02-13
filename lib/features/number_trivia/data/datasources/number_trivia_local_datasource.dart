25f2c3e32ddcf5e509a7c47ff418861d0c380eb5
fuadaghazada <fuad.aghazade@pashabank.az>
2022-02-13 11:51:41 +0400

    Adding data repository implementation

---
+++ lib/features/number_trivia/data/datasources/number_trivia_local_datasource.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_pg/features/number_trivia/data/models/number_trivia_model.dart';
+
+abstract class NumberTriviaLocalDataSource {
+  Future<NumberTriviaModel> getLastNumberTrivia();
+  Future<void> cacheNumberTrivia(NumberTriviaModel numberTriviaModel);
+}
